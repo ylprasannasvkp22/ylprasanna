9a18175a6df2e876a3c0f7657a0949b17ed76e61
LPT-06 <LPT-06@10.0.0.111>
2024-12-12 16:12:07 +0530

    commit1

---
+++ SVKP_college/src/basicsofjava/Ternary_Demo.java
@@ -0,0 +1,12 @@
+package basicsofjava;
+
+public class Ternary_Demo {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int max=(a>b)?a:b;
+		System.out.println("max value is : " + max);
+
+	}
+
+}
