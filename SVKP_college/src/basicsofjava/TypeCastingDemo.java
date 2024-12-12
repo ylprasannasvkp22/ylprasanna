5008e82570eab88c1216e248c07f7a508d34fe87
LPT-06 <LPT-06@10.0.0.111>
2024-12-12 16:14:14 +0530

    commit1

--- SVKP_college/src/basicsofjava/TypeCastingDemo.java
+++ SVKP_college/src/basicsofjava/TypeCastingDemo.java
@@ -3,7 +3,26 @@ package basicsofjava;
 public class TypeCastingDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		     //implicit casting
+				byte b=127;
+		     	int i=b;
+				System.out.println(i);
+				
+			
+				float f1=22.14f;
+				double d1=f1;
+				System.out.println(d1);
+				//Explicit casting
+			
+			    double d2=10.52d;
+				long l=(long)d2;
+				System.out.println(l);
+				
+				
+				float f2=34.56f;
+				int li=(int)f2;
+				System.out.println(li);
+		
 
 	}
 
