d042aa04fdc47662b2b05c9e7aff59e06ec4df06
LPT-06 <LPT-06@10.0.0.111>
2024-12-12 15:58:03 +0530

    commit1

---
+++ SVKP_college/src/basicsofjava/ReturnDemo.java
@@ -0,0 +1,16 @@
+package basicsofjava;
+
+public class ReturnDemo {
+
+	public static void main(String[] args) 
+	{System.out.println(sum(5,10));
+	}
+static int sum(int a,int b)
+{
+	return a+b;
+	
+		
+
+	}
+
+}
