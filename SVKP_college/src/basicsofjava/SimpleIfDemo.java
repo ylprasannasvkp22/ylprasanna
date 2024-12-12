2b0a0285c47b5983e7a8fc03b56e366958b59a98
LPT-06 <LPT-06@10.0.0.111>
2024-12-12 16:01:21 +0530

    commit1

---
+++ SVKP_college/src/basicsofjava/SimpleIfDemo.java
@@ -0,0 +1,17 @@
+package basicsofjava;
+
+public class SimpleIfDemo {
+	public static void main(String[] args) {
+		int age=22;
+		if(age>18)
+		{
+			System.out.println("you are eligible");
+		}
+		else
+		{
+			System.out.println("you are not eligible");
+		}
+	}
+	
+
+}
