e4204bd311996c8c371b5babcb09a9d8f7ac8c4a
LPT-06 <LPT-06@10.0.0.105>
2024-12-16 14:57:18 +0530

    commit1

---
+++ SVKP_college/src/Exception/ArrayIndex.java
@@ -0,0 +1,22 @@
+package Exception;
+
+public class ArrayIndex {
+	public static void main(String[] args) {
+		int arr[]=new int[4]; //4 elements //0-3
+		try
+		{
+			int i=arr[8];
+			  System.out.println(i);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			System.out.println("whatever happen this line gets executed");
+		}
+		  
+	}
+
+}
