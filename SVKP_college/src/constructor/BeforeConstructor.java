7ab712bccd2b19a75627cbd9d2bbd562c7366ba0
LPT-06 <LPT-06@10.0.0.105>
2024-12-16 14:16:21 +0530

    commit1

---
+++ SVKP_college/src/constructor/BeforeConstructor.java
@@ -0,0 +1,16 @@
+package constructor;
+class Myclass
+{
+	String color;
+	int cost;
+	String Brand;
+}
+public class BeforeConstructor {
+	public static void main(String[] args) {
+		Myclass obj=new Myclass();
+		System.out.println(obj.color + " " +obj.cost + " "+obj.Brand);
+
+	}
+	
+
+}
