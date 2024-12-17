4afb99dc7ecce1e1afc31a86d59d9a419a930e17
LPT-06 <LPT-06@10.0.0.108>
2024-12-17 14:51:01 +0530

    commit1

---
+++ SVKP_college/src/Exception/NullPointerExceptions.java
@@ -0,0 +1,18 @@
+package Exception;
+
+public class NullPointerExceptions {
+	public static void main(String[] args) {
+		String str=null;
+		
+		try
+		{
+			System.out.println(str.length());
+		}catch(NullPointerException e)
+		{
+		System.out.println(e);
+		}
+		System.out.println("welcome");
+
+	}
+
+}
