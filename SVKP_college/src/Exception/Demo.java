e0b6090aba1f1b557398038217e7cd903b86c125
LPT-06 <LPT-06@10.0.0.108>
2024-12-17 14:36:43 +0530

    commit1

---
+++ SVKP_college/src/Exception/Demo.java
@@ -0,0 +1,16 @@
+package Exception;
+
+public class Demo {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int sum=add(a,b);
+		
+		System.out.println(sum);
+	}
+	public static int add(int a,int b)
+	{
+		return a-b;
+	}
+
+}
