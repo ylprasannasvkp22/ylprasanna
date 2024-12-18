180b526fafaa44cbaded8d466034ac0f3f953c44
LPT-06 <LPT-06@10.0.0.108>
2024-12-18 14:27:26 +0530

    commit1

---
+++ SVKP_college/src/finaldemo/Finalclass.java
@@ -0,0 +1,23 @@
+package finaldemo;
+ class calc 
+{
+	public void show()
+	{
+		System.out.println("AI integration done by hema");
+	}
+}
+
+class adav extends  calc 
+{
+	public void show()
+	{
+		System.out.println("AI integration done by yasar");
+	}
+}
+
+public class Finalclass {
+public static void main(String[] args) {
+		
+
+	}
+}
