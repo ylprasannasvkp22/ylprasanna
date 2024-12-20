7516c4daf7d154b363dfb659219550ed72260f2d
LPT-06 <LPT-06@10.0.0.102>
2024-12-20 14:25:39 +0530

    commit1

---
+++ SVKP_college/src/jr_developer/KIA_AirBags.java
@@ -0,0 +1,15 @@
+package jr_developer;
+
+public class KIA_AirBags {
+	public void disp_AirBags()
+
+	{
+		System.out.println("All KIA have airbags ");
+	}
+	 
+	public static void main(String[] args)
+	 {
+		 KIA_Speed_limit obj=new KIA_Speed_limit();
+		 obj.disp_Speed_limit();
+	 }
+}
---
+++ SVKP_college/src/jr_developer/KIA_Speed_limit.java
@@ -0,0 +1,13 @@
+package jr_developer;
+
+public class KIA_Speed_limit {
+	 public void disp_Speed_limit()
+		{
+			System.out.println("All KIA have speedlimit ");
+		}
+//	 public static void main(String[] args)
+//	 {
+//		 KIA_Speed_limit obj=new KIA_Speed_limit();
+//		 obj.disp_Speed_limit();
+//	 }	 
+}
