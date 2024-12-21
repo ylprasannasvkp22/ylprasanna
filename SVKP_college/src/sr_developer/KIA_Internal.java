0c9905d85d5314896f096c3246ba6561a71759ab
LPT-06 <LPT-06@10.0.0.103>
2024-12-21 15:16:26 +0530

    commit1

---
+++ SVKP_college/src/sr_developer/KIA_Internal.java
@@ -0,0 +1,13 @@
+package sr_developer;
+import org.tnsif.capgemini.c2tc.jr_developer.*;
+public class KIA_Internal {
+public static void main(String[] args) {
+		
+		KIA_AirBags obj=new KIA_AirBags();
+		obj.disp_AirBags();
+		
+//		 KIA_Internal obj1=new  KIA_Internal();
+//		obj1.disp_Speed_limit();
+
+	}
+}
